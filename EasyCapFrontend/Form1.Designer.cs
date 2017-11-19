2fd0994d5adc14227aed82b6d1ec3112c49e6702
libertyernie <maybeway36@gmail.com>
2017-11-19 17:59:04 -0600

    version 1

---
+++ EasyCapFrontend/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace EasyCapFrontend {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.lblStartTime = new System.Windows.Forms.Label();
+            this.chkImmediate = new System.Windows.Forms.CheckBox();
+            this.dtStartTime = new System.Windows.Forms.DateTimePicker();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.numDuration = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblCRF = new System.Windows.Forms.Label();
+            this.numCrf = new System.Windows.Forms.NumericUpDown();
+            this.lblPreset = new System.Windows.Forms.Label();
+            this.ddlPreset = new System.Windows.Forms.ComboBox();
+            this.lblTune = new System.Windows.Forms.Label();
+            this.ddlTune = new System.Windows.Forms.ComboBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblFfmpegPath2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOutputDir = new System.Windows.Forms.TextBox();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numDuration)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCrf)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblStartTime
+            // 
+            this.lblStartTime.AutoSize = true;
+            this.lblStartTime.Location = new System.Drawing.Point(6, 19);
+            this.lblStartTime.Margin = new System.Windows.Forms.Padding(3);
+            this.lblStartTime.Name = "lblStartTime";
+            this.lblStartTime.Size = new System.Drawing.Size(54, 13);
+            this.lblStartTime.TabIndex = 2;
+            this.lblStartTime.Text = "Start time:";
+            // 
+            // chkImmediate
+            // 
+            this.chkImmediate.AutoSize = true;
+            this.chkImmediate.Location = new System.Drawing.Point(66, 18);
+            this.chkImmediate.Name = "chkImmediate";
+            this.chkImmediate.Size = new System.Drawing.Size(79, 17);
+            this.chkImmediate.TabIndex = 3;
+            this.chkImmediate.Text = "Immedately";
+            this.chkImmediate.UseVisualStyleBackColor = true;
+            this.chkImmediate.CheckedChanged += new System.EventHandler(this.chkImmediate_CheckedChanged);
+            // 
+            // dtStartTime
+            // 
+            this.dtStartTime.CustomFormat = "";
+            this.dtStartTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dtStartTime.Location = new System.Drawing.Point(66, 41);
+            this.dtStartTime.Name = "dtStartTime";
+            this.dtStartTime.ShowUpDown = true;
+            this.dtStartTime.Size = new System.Drawing.Size(100, 20);
+            this.dtStartTime.TabIndex = 4;
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Location = new System.Drawing.Point(6, 69);
+            this.lblDuration.Margin = new System.Windows.Forms.Padding(3);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(50, 13);
+            this.lblDuration.TabIndex = 5;
+            this.lblDuration.Text = "Duration:";
+            // 
+            // numDuration
+            // 
+            this.numDuration.DecimalPlaces = 3;
+            this.numDuration.Location = new System.Drawing.Point(66, 67);
+            this.numDuration.Name = "numDuration";
+            this.numDuration.Size = new System.Drawing.Size(60, 20);
+            this.numDuration.TabIndex = 6;
+            this.numDuration.ThousandsSeparator = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lblStartTime);
+            this.groupBox1.Controls.Add(this.numDuration);
+            this.groupBox1.Controls.Add(this.chkImmediate);
+            this.groupBox1.Controls.Add(this.lblDuration);
+            this.groupBox1.Controls.Add(this.dtStartTime);
+            this.groupBox1.Location = new System.Drawing.Point(12, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(260, 93);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Scheduling";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.ddlTune);
+            this.groupBox3.Controls.Add(this.lblTune);
+            this.groupBox3.Controls.Add(this.ddlPreset);
+            this.groupBox3.Controls.Add(this.lblPreset);
+            this.groupBox3.Controls.Add(this.numCrf);
+            this.groupBox3.Controls.Add(this.lblCRF);
+            this.groupBox3.Location = new System.Drawing.Point(12, 124);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(260, 73);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Encoding";
+            // 
+            // lblCRF
+            // 
+            this.lblCRF.AutoSize = true;
+            this.lblCRF.Location = new System.Drawing.Point(6, 22);
+            this.lblCRF.Name = "lblCRF";
+            this.lblCRF.Size = new System.Drawing.Size(31, 13);
+            this.lblCRF.TabIndex = 0;
+            this.lblCRF.Text = "CRF:";
+            // 
+            // numCrf
+            // 
+            this.numCrf.Location = new System.Drawing.Point(43, 19);
+            this.numCrf.Maximum = new decimal(new int[] {
+            51,
+            0,
+            0,
+            0});
+            this.numCrf.Name = "numCrf";
+            this.numCrf.Size = new System.Drawing.Size(60, 20);
+            this.numCrf.TabIndex = 1;
+            this.numCrf.Value = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            // 
+            // lblPreset
+            // 
+            this.lblPreset.AutoSize = true;
+            this.lblPreset.Location = new System.Drawing.Point(109, 22);
+            this.lblPreset.Name = "lblPreset";
+            this.lblPreset.Size = new System.Drawing.Size(40, 13);
+            this.lblPreset.TabIndex = 2;
+            this.lblPreset.Text = "Preset:";
+            // 
+            // ddlPreset
+            // 
+            this.ddlPreset.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlPreset.FormattingEnabled = true;
+            this.ddlPreset.Items.AddRange(new object[] {
+            "ultrafast",
+            "superfast",
+            "veryfast",
+            "faster",
+            "fast",
+            "medium",
+            "slow",
+            "slower",
+            "veryslow"});
+            this.ddlPreset.Location = new System.Drawing.Point(154, 19);
+            this.ddlPreset.Name = "ddlPreset";
+            this.ddlPreset.Size = new System.Drawing.Size(100, 21);
+            this.ddlPreset.TabIndex = 3;
+            // 
+            // lblTune
+            // 
+            this.lblTune.AutoSize = true;
+            this.lblTune.Location = new System.Drawing.Point(6, 49);
+            this.lblTune.Name = "lblTune";
+            this.lblTune.Size = new System.Drawing.Size(81, 13);
+            this.lblTune.TabIndex = 4;
+            this.lblTune.Text = "Tune (optional):";
+            // 
+            // ddlTune
+            // 
+            this.ddlTune.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ddlTune.FormattingEnabled = true;
+            this.ddlTune.Items.AddRange(new object[] {
+            "",
+            "film",
+            "animation",
+            "grain",
+            "stillimage",
+            "fastdecode",
+            "zerolatency"});
+            this.ddlTune.Location = new System.Drawing.Point(93, 46);
+            this.ddlTune.Name = "ddlTune";
+            this.ddlTune.Size = new System.Drawing.Size(161, 21);
+            this.ddlTune.TabIndex = 5;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStart.Location = new System.Drawing.Point(197, 286);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblFfmpegPath2
+            // 
+            this.lblFfmpegPath2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblFfmpegPath2.AutoEllipsis = true;
+            this.lblFfmpegPath2.Location = new System.Drawing.Point(12, 9);
+            this.lblFfmpegPath2.Name = "lblFfmpegPath2";
+            this.lblFfmpegPath2.Size = new System.Drawing.Size(260, 13);
+            this.lblFfmpegPath2.TabIndex = 0;
+            this.lblFfmpegPath2.Text = "not found";
+            this.lblFfmpegPath2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(132, 69);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "minutes";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.txtFilename);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txtOutputDir);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 203);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 71);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Output";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Directory:";
+            // 
+            // txtOutputDir
+            // 
+            this.txtOutputDir.Location = new System.Drawing.Point(64, 19);
+            this.txtOutputDir.Name = "txtOutputDir";
+            this.txtOutputDir.Size = new System.Drawing.Size(190, 20);
+            this.txtOutputDir.TabIndex = 1;
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(64, 45);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(190, 20);
+            this.txtFilename.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Filename:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 321);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.lblFfmpegPath2);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numDuration)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numCrf)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblStartTime;
+        private System.Windows.Forms.CheckBox chkImmediate;
+        private System.Windows.Forms.DateTimePicker dtStartTime;
+        private System.Windows.Forms.Label lblDuration;
+        private System.Windows.Forms.NumericUpDown numDuration;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblCRF;
+        private System.Windows.Forms.NumericUpDown numCrf;
+        private System.Windows.Forms.Label lblPreset;
+        private System.Windows.Forms.ComboBox ddlPreset;
+        private System.Windows.Forms.ComboBox ddlTune;
+        private System.Windows.Forms.Label lblTune;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblFfmpegPath2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOutputDir;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ EasyCapFrontend/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EasyCapFrontend {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+
+            Enabled = false;
+            chkImmediate.Checked = true;
+            ddlPreset.SelectedIndex = ddlPreset.Items.IndexOf("slow");
+
+            txtOutputDir.Text = Environment.CurrentDirectory;
+            txtFilename.Text = DateTime.UtcNow.ToString("u").Replace(':', '-').Replace(' ', '_');
+
+            foreach (string path in new[] {
+                Environment.CurrentDirectory,
+                Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)
+            }.Concat(Environment.GetEnvironmentVariable("PATH").Split(Path.PathSeparator))) {
+                string p = path;
+                while (p.EndsWith("\"")) {
+                    p = path.Substring(0, path.Length - 1);
+                }
+                string f = Path.Combine(p, "ffmpeg.exe");
+                if (File.Exists(f)) {
+                    lblFfmpegPath2.Text = f;
+                    Enabled = true;
+                    break;
+                }   
+            }
+        }
+
+        private void chkImmediate_CheckedChanged(object sender, EventArgs e) {
+            dtStartTime.Enabled = !chkImmediate.Checked;
+        }
+
+        private async void btnStart_Click(object sender, EventArgs e) {
+            Enabled = false;
+
+            try {
+                DateTime startTime = DateTime.Today + dtStartTime.Value.TimeOfDay;
+                if (startTime < DateTime.Now) startTime += TimeSpan.FromDays(1);
+                if (chkImmediate.Checked) startTime = DateTime.Now;
+
+                string filename = txtFilename.Text;
+                if (!filename.EndsWith(".mp4", StringComparison.CurrentCultureIgnoreCase)) {
+                    filename += ".mp4";
+                }
+                string filepath = Path.Combine(txtOutputDir.Text, filename);
+
+                string audio = null;
+                string video = null;
+
+                var psi1 = new ProcessStartInfo(lblFfmpegPath2.Text, $"-f dshow -list_devices true -i dummy") {
+                    UseShellExecute = false,
+                    RedirectStandardError = true
+                };
+                var p1 = Process.Start(psi1);
+                await Task.Run(() => {
+                    using (var sr = p1.StandardError) {
+                        string line;
+                        string mode = null;
+                        while ((line = sr.ReadLine()) != null) {
+                            if (line.Contains("DirectShow video devices")) {
+                                mode = "video";
+                            } else if (line.Contains("DirectShow audio devices")) {
+                                mode = "audio";
+                            } else if (line.Contains("USB Video Device") || line.Contains("USB Audio Device")) {
+                                line = line.Substring(line.IndexOf('"') + 1);
+                                line = line.Substring(0, line.LastIndexOf('"'));
+                                if (line.Length > 0) {
+                                    Console.WriteLine(line);
+                                    if (mode == "video") video = line;
+                                    if (mode == "audio") audio = line;
+                                }
+                            }
+                        }
+                    }
+                    p1.WaitForExit();
+                });
+
+                var psi2 = new ProcessStartInfo(
+                    lblFfmpegPath2.Text,
+                    $"-t {numDuration.Value * 60} -f dshow -video_size 720x240 -framerate 30 " +
+                    $"-pixel_format yuyv422 -i video=\"{video}\":audio=\"{audio}\" " +
+                    $"-f mp4 -pix_fmt yuv420p -s 720x540 -c:v libx264 -preset {ddlPreset.Text} -crf {(int)numCrf.Value} -c:a libmp3lame -qscale:a 0 " +
+                    $"\"{filepath}\""
+                ) {
+                    UseShellExecute = false,
+                    RedirectStandardError = true
+                };
+
+                var ts = startTime - DateTime.Now;
+                if (ts < TimeSpan.Zero) ts = TimeSpan.Zero;
+
+                await Task.Delay(ts);
+
+                if (File.Exists(filepath)) File.Delete(filepath);
+
+                var p2 = Process.Start(psi2);
+                await Task.Run(() => {
+                    using (var sr = p2.StandardError) {
+                        string line;
+                        while ((line = sr.ReadLine()) != null) {
+                            Console.Error.WriteLine(line);
+                        }
+                    }
+                    p2.WaitForExit();
+                });
+            } catch (Exception ex) {
+                Console.Error.WriteLine(ex.Message + ex.StackTrace);
+                MessageBox.Show(this, ex.Message);
+            }
+
+            Enabled = true;
+        }
+    }
+}
